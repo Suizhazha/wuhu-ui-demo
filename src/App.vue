56e20bd7237a3d19e8365b8c6c03a0366075841f
Suizhazha <573505403@qq.com>
2020-08-04 20:07:49 +0800

    è创建ä完成sui第ä¸ä成第ä完成第一个ésuixin组件ââ

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>hi</div>
+  <suixin/>
+</template>
++++++
+<script>
+import suixin from './components/suixin.vue'
+
+export default {
+  name: 'App',
+  components: {
+    suixin
+  }
+}
+</script>
